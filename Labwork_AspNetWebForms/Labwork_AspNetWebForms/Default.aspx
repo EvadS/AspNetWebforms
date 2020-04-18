722d2d2210359dd26962dd405c0a76642e981cd4
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2020-04-18 11:25:56 +0300

    first task

---
+++ Labwork_AspNetWebForms/Labwork_AspNetWebForms/Default.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MyMaster.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Labwork_AspNetWebForms.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
---
+++ Labwork_AspNetWebForms/Labwork_AspNetWebForms/MyMaster.Master.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Labwork_AspNetWebForms
+{
+    public partial class MyMaster : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Выполнять инициализацию, только если страница запрашивается
+            // впервые.
+            // После этого данная информация отслеживается в состоянии вида,
+            if (!this.Page.IsPostBack)
+            {
+                // Установить атрибуты стиля для настройки внешнего вида.
+                TextBox1.Style["font-size"] = "20px";
+                TextBox1.Style["color"] = "red";
+                // Использовать слегка отличающийся, но эквивалентный синтаксис
+                // для установки атрибута стиля.
+                TextBox1.Style.Add("background-color", "lightyellow");
+                // Установить текст по умолчанию.
+                TextBox1.Text = "<Enter e-mail address here>";
+                // Установить/ другие нестандартные атрибуты.
+                TextBox1.Attributes["onfocus"] = "alert(TextBoxl.value)";
+            }
+        }
+    }
+}
